ad37f2a0b17da1b8b35420bcf932fd3016ec0452
pedrocruz <JayJMarquez05@gmail.com>
2024-11-07 14:41:22 -0600

    File structure is Compelte

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const dotenv = require("dotenv");
+const dotenv = require(`dotenv`);
 dotenv.config();
-const express = require("express");
+const express = require(`express`);
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require(`mongoose`);
+const cors = require(`cors`);
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on("connected", () => {
+mongoose.connection.on(`connected`, () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.json());
 
-// Routes go here
-
 app.listen(3000, () => {
-  console.log("The express app is ready!");
+  console.log(`The express app is ready!`);
 });
